9d33a8865bfeb4d86fff540477324539f383c71d
Gersen James <gersen.jorge@gmail.com>
2019-02-12 10:49:25 -0200

    Criando Repository e Service para categoria

---
+++ src/main/java/com/gersen/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.gersen.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gersen.cursomc.domain.Categoria;
+import com.gersen.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) { 
+		  Optional<Categoria> obj = repo.findById(id); 
+		  return obj.orElse(null); 
+		} 
+
+}
